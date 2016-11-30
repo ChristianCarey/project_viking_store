3bb805ad62e7f60e4868f0b486bca71619996b69
Pramod Jacob <psjacob68@gmail.com>
2016-11-30 15:01:38 -0500

    working on panel 2

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,17 @@
 class Location < ApplicationRecord
-  def
 
+  include ActionView::Helpers::TextHelper
+
+  def self.most_frequent(num=3)
+    # SELECT class.name, COUNT(*) AS state_count
+    # FROM users JOIN addresses ON users.billing_id = class.id
+    # GROUP BY class.id
+    # ORDER BY state_count DESC
+    # LIMIT num
+    User.select("#{pluralize(name)}.name, COUNT(*)")
+      .joins("JOIN addresses ON users.billing_id = #{name}.id")
+      .group("#{name}.id")
+      .order('count DESC')
+      .limit(num.to_s)
   end
 end

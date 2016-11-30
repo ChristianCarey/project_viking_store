9c0284801566318ccc0451cf89e2cf7e167c4ceb
Pramod Jacob <psjacob68@gmail.com>
2016-11-30 13:36:58 -0500

    finished panel 1

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -2,11 +2,11 @@ class OrderContent < ApplicationRecord
   def self.total_revenue
     # for each row, multiply quantity by product_id's product's price
     # sum of total cost for each row
-    total_costs.sum
+    with_products.sum('quantity * products.price')
   end
 
-  def self.total_costs
+  def self.with_products
     # returns table of OrderContent primary key and total cost
-    select('order_id, product_id, quantity * products.price AS total_cost').joins('JOIN products ON product_id=products.id').to_sql
+    select('*').joins('JOIN products ON product_id=products.id')
   end
 end
